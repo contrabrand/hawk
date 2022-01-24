fc57a6d522391b9f1a94a9ea195bb69cbe736f0e
contrabrand <86887091+contrabrand@users.noreply.github.com>
2022-01-24 16:23:39 +0700

    Update vpn.php

--- vpn.php
+++ vpn.php
@@ -1,11 +1,11 @@
-{"Version":"1.07",
+{"Version":"1.08",
 
 "Url":"https://github.com/contrabrand/hawk/raw/main/hawk.zip" ,
 
 "Changelog":"  
 
 Thankfully for what is done and looking forward to the next step of life and become more grateful to the God
-
+♥️♥️
 ",
 
 "latestVersion": "1.0.0",
